f76b854ba17e9a0ea178e961785224e00936e3c0
Ste2038 <giulianelliste@gmail.com>
2018-03-12 14:35:15 +0100

    aggiunta ActionsOnGoogle

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ var app = require('express')();
 var basicAuth = require('express-basic-auth');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const functions = require('firebase-functions');
+const DialogflowApp = require('actions-on-google').DialogflowApp;
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+  if (request.body.result) {
+    //processV1Request(request, response);
+  } else if (request.body.queryResult) {
+    //processV2Request(request, response);
+  } else {
+    console.log('Invalid Request');
+    return response.status(400).end('Invalid Webhook Request (expecting v1 or v2 webhook request)');
+  }
+});
 
 app.use(basicAuth({
   users: { 'admin': 'secret'}
