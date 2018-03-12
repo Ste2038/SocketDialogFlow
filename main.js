19d695a1f0e3afb7d43915c31fe3f811e02783eb
Ste2038 <giulianelliste@gmail.com>
2018-03-12 16:37:28 +0100

    Prova Request

--- main.js
+++ main.js
@@ -30,9 +30,8 @@ app.get('/', function(req, res){
 
 app.post('/', function(req, res){
   console.log('POST / ', JSON.stringify(req.body.originalRequest.data.user.userId));
-  io.emit('chat message', 'porta');
   io.emit('chatId', JSON.stringify(req.body.originalRequest.data.user.userId));
-  
+  io.emit('chat message', 'porta');
   res.send(req.body);
 });
 
