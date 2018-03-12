a531aaf396bbc5459c9acd94541ae87264aaac1b
Ste2038 <giulianelliste@gmail.com>
2018-03-12 11:50:21 +0100

    analisi req

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ app.get('/', function(req, res){
 app.post('/', function(req, res){
   console.log('POST /');
   io.emit('chat message', 'porta');
-  io.emit('log', req);
+  io.emit('log', JSON.stringify(req));
   console.log("a");
   console.log(req);
   res.send("Porta");
