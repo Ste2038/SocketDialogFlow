4623e93308353a25bb53c14627b8b1cbe654d038
Ste2038 <giulianelliste@gmail.com>
2018-03-13 11:30:31 +0100

    Correzioni

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ app.get('/', function(req, res){
 });
 
 app.post('/', function(req, res){
-  console.log('POST / ', JSON.stringify(req));
+  console.log('POST / ', JSON.stringify(req.body));
   io.emit('chatId', JSON.stringify(req.body.originalRequest.data.user.userId));
   io.emit('chat message', 'porta');
   res.send(req.body);
